e176eef87449f3f5fd970393602413d13717be34
Andrew Kaiser <kaisea@rpi.edu>
2015-10-06 17:09:33 -0400

    changed theme, added sass packages

--- client/app/app.js
+++ client/app/app.js
@@ -1,5 +1,12 @@
 angular.module('telebumApp', ['ui.router', 'ngMaterial'])
-  .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+  .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $mdThemingProvider) {
     $urlRouterProvider.otherwise('/')
     $locationProvider.html5Mode(true);
+
+    $mdThemingProvider
+      .theme('green')
+      .primaryPalette('green', {
+        'default': '600', // by default use shade 900 from the grey palette for primary intentions
+      })
+      $mdThemingProvider.setDefaultTheme('green');
   })
