890b541d8ddbafc488c1a15292fab18c39d99a79
Andrew Kaiser <kaisea@rpi.edu>
2015-10-24 22:20:45 -0400

    refresh browser w/out extension

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,38 @@
 // Load all the plugins (Seen in package.json dev-dependencies)
-var gulp 		= require('gulp');
-var minifyCSS 	= require('gulp-minify-css');    // minifies css files
-var rename 		= require('gulp-rename');          // renames files
-var jshint 		= require('gulp-jshint');				   // Checks js files for errors ie. semi colons
-var concat		= require('gulp-concat');				   // Converts multiple files into one file
-var uglify 		= require('gulp-uglify');				   // stringifies js files
-var ngAnnotate 	= require('gulp-ng-annotate'); 	 // Makes angular files able to be combined into one file
-var nodemon 	= require('gulp-nodemon');				 // Watches for changing files and restarts
+// var minifyCSS   = require('gulp-minify-css');    // minifies css files
+// var rename      = require('gulp-rename');          // renames files
+// var jshint      = require('gulp-jshint');				   // Checks js files for errors ie. semi colons
+// var concat      = require('gulp-concat');				   // Converts multiple files into one file
+// var uglify      = require('gulp-uglify');				   // stringifies js files
+// var ngAnnotate  = require('gulp-ng-annotate'); 	 // Makes angular files able to be combined into one file
+// var bower  = require('gulp-bower'); 	 // run bower install
+var gulp      = require('gulp'),
+  path        = require('path'),
+  nodemon     = require('gulp-nodemon'),				 // Watches for changing files and restarts
+  sass        = require('gulp-sass'),
+  lr          = require('tiny-lr')()
+
+// move font awesome to
+gulp.task('icons', function() { 
+    return gulp.src('./clent/bower_components/fontawesome/fonts/**.*') 
+        .pipe(gulp.dest('./client/fonts')); 
+});
 
 // task to minify css files
-gulp.task('css', function(){
+
+// task to convert sass to css
+gulp.task('styles', function() {
+    return gulp.src('./client/app/app.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./client/assets/css/'));
+    // gulp.src('./client/bower_components/Skeleton-Sass/scss/skeleton.scss')
+    //     .pipe(sass().on('error', sass.logError))
+    //     .pipe(gulp.dest('./client/assets/css/'));
 
 });
 
 // task for linting js files
 gulp.task('js', function(){
-
 	// grab all the files
 	// return gulp.src([	'server/**/*.js',	'client/**/*.js'])
 		// .pipe(jshint())
@@ -31,28 +48,75 @@ gulp.task('angular', function(){
 	// 	.pipe(gulp.dest('public/dist'));
 });
 
+
+function notifyLivereload(event) {
+  if (event) {
+    console.log("reloading browser for file " + path.basename(event.path));
+    lr.changed({
+      body: {
+        files: [event.path]
+      }
+    });
+  } else {
+    console.log('did it')
+    lr.changed({
+      body: {
+        files: [path.join('./client/index.html')]
+      }
+    })
+  }
+}
+
 gulp.task('watch',function() {
-
 	// watch js files and run lint and run js and angular tasks
-	gulp.watch(	['server/**/*.js', 'client/**/*.js'],
-				['js', 'angular']);
+	// gulp.watch(	['server/**/*.js'],				['js', 'angular']);
 
-	// watch css files and run css task to minify
-	// gulp.watch(	['client/assets/css/*.css'],['css']);
+  // watch client pages and reload the browser
+  gulp.watch(['client/**/*.html'], notifyLivereload)
+  gulp.watch(['client/**/*.js'], notifyLivereload)
+  gulp.watch(['client/assets/css/*.css'], notifyLivereload)
+  gulp.watch(['client/assets/images/*.png'], notifyLivereload)
+  gulp.watch(['client/assets/images/*.jpg'], notifyLivereload)
+  gulp.watch(['client/assets/images/*.gif'], notifyLivereload)
+  // watch server pages and reload the browser after nodemon executes
+  gulp.watch(['server/**/*.js'], function (event) {
+    // waits until the server restarts to execute
+    setTimeout(function () {
+      notifyLivereload(event)
+    }, 1000)
+  });
+  // compile the sass to css
+  gulp.watch( ['client/app/**/*.scss'], ['styles'])
+  // watch css files and run css task to minify
+  // gulp.watch(	['client/assets/css/*.css'],['css']);
 
 });
+// port for the brower livereload
+gulp.task('tiny', function () {
+  lr.listen(35729);
+})
 
 // the nodemon task
 gulp.task('nodemon',function() {
 	nodemon({
 		script:'server/server.js',
-		ext:'js'
+		ext:'js',
+    watch: 'server'
 	})
-	.on('start', ['watch'])
-	.on('change', ['watch'])
+  .on('start', function () {
+
+  })
 	.on('restart',function() {
 		console.log("restarted");
-	});
+    // notifyLivereload()
+	})
 });
 
-gulp.task('default', ['nodemon']);		// Just run gulp!
+// once gulp 4.0 arrives, do tasks in this order:
+// [nodemon, tiny]
+// [styles, minification]
+// watch
+gulp.task('default', ['nodemon', 'tiny', 'styles', 'watch']);		// Just run gulp!
+
+
+// need to handle errors, make sure nodemon exits
