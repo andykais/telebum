8fc87c14047782d2efabe52e7e1251d63ebfe561
Andrew Kaiser <kaisea@rpi.edu>
2015-11-03 17:57:29 -0500

    advancing watches season,percentBar

--- client/app/shows/shows.controller.js
+++ client/app/shows/shows.controller.js
@@ -5,36 +5,72 @@ angular.module('telebumApp')
     angular.element(document).ready(function () {
       for (var seriesId in showData) {
         var show = showData[seriesId];
-        var percent = parseInt(show.episodesSeen)/parseInt(show.totalEpisodes)*100;
-        percent = Math.round(percent);
-
-        var showModel = document.getElementById(seriesId);
-        var bar = showModel.childNodes[1].childNodes[1];
-        bar.style.width=percent + "%";
+          changePercentage(show, seriesId)
       }
-    })
-    $scope.advance = function(id) {
+    });
+
+    $scope.advance = function (id) {
       var show = showData[id];
-      show.episode ++;
-      console.log(show)
+      var episodeNum = show.on.episode;
+      var seasonNum = show.on.season;
+      var numPerSeason = show.released[seasonNum - 1];
+      var totalEpisodes = show.totalEpisodes;
+      var totalSeasons = show.released.length;
+      var seenEpisodes = show.seen.episodes;
+
+      if (seasonNum == totalSeasons && episodeNum >= numPerSeason) {
+        // do nothing for now
+      } else if (episodeNum >= numPerSeason) {
+        episodeNum = 1;
+        seasonNum ++;
+        seenEpisodes ++;
+      } else {
+        episodeNum ++;
+        seenEpisodes ++;
+      }
+      console.log(seenEpisodes)
+      show.on.episode = episodeNum;
+      show.on.season = seasonNum;
+      show.seen.episodes = seenEpisodes;
+      changePercentage(show, id)
+      // show.released[seasonNum] = numPerSeason;
     }
   });
 
+function changePercentage(show, seriesId) {
+  var percent = parseInt(show.seen.episodes)/parseInt(show.totalEpisodes)*100;
+  percent = Math.round(percent);
+
+  var showModel = document.getElementById(seriesId);
+  var bar = showModel.childNodes[1].childNodes[1];
+  bar.style.width=percent + "%";
+
+}
 
 //temporary data until joey gets the main api call working
 var showData = {
   "183122": {
     "title": "Adventure Time",
-    "season": 2,
-    "episode": 24,
-    "episodesSeen": 34,
-    "totalEpisodes": 108
+    "released": [22,22,28,30,20,43],
+    "totalEpisodes": 165,
+    "on": {
+      "season": 2,
+      "episode": 21
+    },
+    "seen": {
+      "episodes": 44
+    }
   },
   "652123": {
     "title": "House of Cards",
-    "season": 3,
-    "episode": 14,
-    "episodesSeen": 26,
-    "totalEpisodes": 45
+    "released": [15,20],
+    "totalEpisodes": 35,
+    "on": {
+      "season": 1,
+      "episode": 14
+    },
+    "seen": {
+      "episodes": 14
+    }
   }
 }
