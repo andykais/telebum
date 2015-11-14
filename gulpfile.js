b3aa3f0ba9087aa61a0924e0d1067609e19bc8f9
Andrew Kaiser <kaisea@rpi.edu>
2015-11-14 14:41:06 -0500

    gulp can seed

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,8 @@ var gulp      = require('gulp'),
   path        = require('path'),
   nodemon     = require('gulp-nodemon'),				 // Watches for changing files and restarts
   sass        = require('gulp-sass'),
-  lr          = require('tiny-lr')()
+  lr          = require('tiny-lr')(),
+  exec = require('child_process').exec;
 
 // move font awesome to
 gulp.task('icons', function() { 
@@ -94,6 +95,13 @@ gulp.task('watch',function() {
 gulp.task('tiny', function () {
   lr.listen(35729);
 })
+// seed the database
+gulp.task('reseed', function () {
+  exec('node server/config/seed.js', function (err, stdout, stderr) {
+    console.log(err)
+  })
+});
+
 
 // the nodemon task
 gulp.task('nodemon',function() {
@@ -115,7 +123,7 @@ gulp.task('nodemon',function() {
 // [nodemon, tiny]
 // [styles, minification]
 // watch
-gulp.task('default', ['nodemon', 'tiny', 'styles', 'watch']);		// Just run gulp!
+gulp.task('default', ['reseed', 'nodemon', 'tiny', 'styles', 'watch']);		// Just run gulp!
 
 
 // need to handle errors, make sure nodemon exits
